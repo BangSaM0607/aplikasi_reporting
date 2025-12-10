1b884862cc7a067b35370ec747f86873ba92e019
Misbah <pendosa427@gmail.com>
2025-12-10 22:00:15 +0700

    Update report_model.dart

---
+++ lib/models/report_model.dart
@@ -0,0 +1,25 @@
+class ReportModel {
+  final String id;
+  final String title;
+  final String description;
+  final String imageUrl;
+  final String createdAt;
+
+  ReportModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+    required this.createdAt,
+  });
+
+  factory ReportModel.fromJson(Map<String, dynamic> json) {
+    return ReportModel(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      imageUrl: json['image_url'],
+      createdAt: json['created_at'],
+    );
+  }
+}
