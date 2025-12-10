c4f33b48aa9e4a6370c68e91fa9a6287568f418a
Misbah <pendosa427@gmail.com>
2025-12-10 21:59:07 +0700

    Update app.dart

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'pages/login_page.dart';
+import 'pages/home_page.dart';
+import 'supabase_config.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final session = supabase.auth.currentSession;
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: session == null ? const LoginPage() : const HomePage(),
+    );
+  }
+}
