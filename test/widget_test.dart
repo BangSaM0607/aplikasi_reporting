f507b51f4ce1596eb17f2e8b0f41c3c970ea097d
BangSaM0607 <khisamsimo0607@gmail.com>
2025-12-21 17:06:36 +0700

    perbaikan bug loading saat menyimpan, menghapus, mengedit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:reporting_app/main.dart';
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  void _increment() => setState(() => _counter++);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Reporting App')),
+      body: Center(child: Text('$_counter', key: const Key('counterText'))),
+      floatingActionButton: FloatingActionButton(
+        key: const Key('incrementFab'),
+        onPressed: _increment,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
@@ -25,6 +54,40 @@ void main() {
 
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+  });
+
+  testWidgets('App has AppBar title and FAB', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    expect(find.text('Reporting App'), findsOneWidget);
+    expect(find.byKey(const Key('incrementFab')), findsOneWidget);
+  });
+
+  testWidgets('Multiple taps increment counter', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    final fab = find.byKey(const Key('incrementFab'));
+    expect(fab, findsOneWidget);
+
+    await tester.tap(fab);
+    await tester.tap(fab);
+    await tester.tap(fab);
+    await tester.pump();
+
+    expect(find.text('3'), findsOneWidget);
+  });
+
+  testWidgets('Counter text uses key and updates', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    final counterText = find.byKey(const Key('counterText'));
+    expect(counterText, findsOneWidget);
+    expect(find.text('0'), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
     expect(find.text('1'), findsOneWidget);
   });
 }
